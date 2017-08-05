a60a69d39859bf4cc005683befe3af3a198ed36a
eprystupa <eugene.prystupa@gmail.com>
2017-08-05 16:08:11 -0400

    Support for react-router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+const Home = () =>
+    <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+    </p>;
+
+const Clients = () =>
+    <div>
+        <h1>Clients</h1>
+        <p><Link to='/clients/eugene'>Eugene</Link></p>
+        <p><Link to='/clients/olga'>Olga</Link></p>
+        <Route path="/clients/:client" component={Client}/>
+    </div>;
+
+const Client = ({ match }) =>
+    <div>Client view: {match.params.client}</div>;
+
+const App = () =>
+    <Router>
+        <div className="App">
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <div>
+                    <Link to="/">Home</Link>
+                    &nbsp;
+                    <Link to="/clients">Clients</Link>
+                </div>
+            </div>
+            <Route exact path="/" component={Home}/>
+            <Route path="/clients" component={Clients}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+    </Router>;
 
 export default App;
